f917fd8d86a4c6fd1706d4bd4b2f0be51e70eb5f
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-02-18 10:45:31 +0100

    Add files via upload

---
+++ branches/mod/vencord6686/meta.js
@@ -0,0 +1,26 @@
+import { createWriteStream, mkdirSync } from "fs";
+import { rm } from "fs/promises";
+import { join } from "path";
+import { Writable } from "stream";
+
+export const name = "Vencord";
+export const description = "Injects Vencord; This is not an officially supported Vencord install method";
+export const incompatibilities = [];
+
+export async function setup(target, log) {
+	const releaseUrl = "https://github.com/6686-repos/Vencord6686/releases/download/devbuild/";
+
+	mkdirSync(join(target, "vencord-desktop"), { recursive: true });
+
+	for (const f of ["vencordDesktopMain.js", "vencordDesktopPreload.js", "renderer.js", "vencordDesktopRenderer.css"]) {
+		log(`Downloading ${f}...`);
+
+		const p = join(target, "vencord-desktop", f);
+		await rm(p, { force: true });
+
+		const req = await fetch(releaseUrl + f);
+		await req.body.pipeTo(Writable.toWeb(createWriteStream(p)));
+	}
+
+	log("Done!");
+}
---
+++ branches/mod/vencord6686/preload.js
@@ -0,0 +1,9 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const { webFrame } = require("electron");
+
+// run vencord's preload
+require("./vencord-desktop/vencordDesktopPreload.js");
+
+// inject vencord renderer
+webFrame.top.executeJavaScript(readFileSync(join(__dirname, "vencord-desktop/renderer.js"), "utf8"));
