67074cd422c29138599adfbf5937b668d2ca4dee
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-03-16 13:12:11 +0100

    add equicord6686

---
+++ branches/mod/equicord6686/meta.js
@@ -0,0 +1,25 @@
+import { createWriteStream, mkdirSync } from "fs";
+import { rm } from "fs/promises";
+import { join } from "path";
+import { Writable } from "stream";
+
+export const name = "Equicord6686";
+export const description = "Injects Equicord6686 (Equicord Fork); This is the only officially supported Equicord6686 install method";
+
+export async function setup(target, log) {
+	const releaseUrl = "https://github.com/6686-repos/Equicord/releases/download/latest/";
+	
+	mkdirSync(join(target, "equicord-desktop"), { recursive: true });
+
+	for (const f of ["equicordDesktopMain.js", "equicordDesktopPreload.js", "renderer.js", "renderer.css"]) {
+		log(`Downloading ${f}...`);
+
+		const p = join(target, "equicord-desktop", f);
+		await rm(p, { force: true });
+
+		const req = await fetch(releaseUrl + f);
+		await req.body.pipeTo(Writable.toWeb(createWriteStream(p)));
+	}
+
+	log("Done!");
+}
