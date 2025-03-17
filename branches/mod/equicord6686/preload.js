22d7aecb95c2411161f50690c8b6f77a18f9976f
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-03-17 14:23:42 +0100

    Update preload.js

--- branches/mod/equicord6686/preload.js
+++ branches/mod/equicord6686/preload.js
@@ -3,7 +3,7 @@ const { join } = require("path");
 const { webFrame } = require("electron");
 
 // run equicord's preload
-require("./equicord-desktop/equicordDesktopPreload.js");
+require("./equicord6686-desktop/equicordDesktopPreload.js");
 
 // inject equicord renderer
-webFrame.top.executeJavaScript(readFileSync(join(__dirname, "equicord-desktop/renderer.js"), "utf8"));
+webFrame.top.executeJavaScript(readFileSync(join(__dirname, "equicord6686-desktop/renderer.js"), "utf8"));
